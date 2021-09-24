56e1d091963782de6795d376eedd76a39993cc3a
Karen Avelar <karen.avelar@outlook.com>
2021-09-24 00:09:20 -0300

    Criando formulário de doação e popup de confirmação

--- AdocaoGatos/src/app/home/home.page.ts
+++ AdocaoGatos/src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MenuController, NavController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -10,15 +11,30 @@ export class HomePage {
   nome = 'Doar'
   opcao = false
 
-  constructor(public nav: NavController) {}
+  constructor(
+    public nav: NavController,
+    public alert: AlertController
+  ) {}
 
   abrirPagina(){
     this.nav.navigateForward('formulario');
+  }
 
+  async popupSalvar(){
+    const alert = await this.alert.create({
+      cssClass: 'my-custom-class',
+      header: 'Obrigada!',
+      subHeader: 'Seu gatinho vai encontrar um lar cheio de amor!',
+      message: 'Entraremos em contato dentro de 2 dias úteis :)',
+      buttons: ['OK']
+    });
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
   }
 
   togle(){
     this.nome = (this.opcao == true) ? 'Doar' : 'Adotar'
   }
-
 }
