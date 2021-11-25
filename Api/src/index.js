3d10d21ad0f84e7b8ccb0dd1aeff302a34a9d689
Gustavo Munis de Moraes <gustavomm29@hotmail.com>
2021-11-25 13:24:35 -0300

    cors'

--- Api/src/index.js
+++ Api/src/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
+
+const cors = require('cors')
 const express = require('express')
 const rota = require('./controller')
 const app = express()
 
 const porta = process.env.PORT || 3100
 
+app.use(cors())
 app.use(express.json())
 app.use('/', rota)
 app.listen(porta)
