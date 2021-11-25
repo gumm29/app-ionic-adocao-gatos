a64c743499175b2f8c76b074702f00fc17cbf247
Gustavo Munis de Moraes <gustavomm29@hotmail.com>
2021-11-25 12:16:06 -0300

    ajuste

--- Api/src/config/sequelize.js
+++ Api/src/config/sequelize.js
@@ -1,6 +1,17 @@
 const Sequelize = require('sequelize')
-const config = require('./bd')
 
-const sequelize = new Sequelize(config)
+if(process.env.NODE_ENV != 'production'){
+  const config = require('./bd')
+  sequelize = new Sequelize(config)
+}else{
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false
+      }
+    }})
+}
+//console.log(config)
+//const sequelize = new Sequelize(config)
 
 module.exports = sequelize
--- Api/src/index.js
+++ Api/src/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const rota = require('./controller')
 const app = express()
