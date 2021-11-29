e5dfe352778872271bc87eccf0465026be3d5ec7
Matheus da Silva Carvalho <matheus.silva.c@hotmail.com>
2021-11-29 20:27:00 -0300

    app - put

--- AdocaoGatos/src/app/util/api.ts
+++ AdocaoGatos/src/app/util/api.ts
@@ -10,4 +10,16 @@ export default class Api{
     const resposta = await fetch(`${this.url}/gato/${id}`)
     return await resposta.json()
   }
+  async atualizar(id: number): Promise<void> {
+    const produtoAtualizado = {
+
+  };
+
+  const body = Object.keys(produtoAtualizado)
+  .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(produtoAtualizado[k])}`)
+  .join('&');
+
+  await fetch(`${this.url}/${id}`, { method: 'PUT', body: new URLSearchParams(body) });
+  this.busca();
+  }
 }
