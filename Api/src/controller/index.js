f2a3cc36122f556b3d03fe0a46d997219c648969
Gustavo Munis de Moraes <gustavomm29@hotmail.com>
2021-12-07 14:57:19 -0300

    testes

--- Api/src/controller/index.js
+++ Api/src/controller/index.js
@@ -34,7 +34,7 @@ rota.put('/gato/:id', async (req, res) =>{
 rota.delete('/gato/:id', async (req, res) =>{
   const id = req.params.id
   const gato = await gatoService.deletar(id)
-  return await gato ? res.status(200).send(gato) : res.status(404).send()
+  return await gato ? res.status(200).send(gato) : res.status(404).send() // arrumar logica esta dando 404 sempre
 })
 
 module.exports = rota
--- Api/src/index.js
+++ Api/src/index.js
@@ -7,7 +7,7 @@ const app = express()
 
 const porta = process.env.PORT || 3100
 
-app.use(cors())
+app.use(cors({ origin: '*' , credentials :  true}))
 app.use(express.json())
 app.use('/', rota)
 app.listen(porta)
